91b758ac9dcdc8a9f263fbcc7808c5b000b0166c
danchecker <35084268+danchecker@users.noreply.github.com>
2023-01-31 13:42:36 -0500

    adding in code to ensure that u does not surpass 255

--- findingwheelspeed_W4A1/findingwheelspeed_W4A1.ino
+++ findingwheelspeed_W4A1/findingwheelspeed_W4A1.ino
@@ -1,9 +1,24 @@
 void setup() {
   // put your setup code here, to run once:
-
+int A = 6; 
+//adding more comments to demonstrate
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  short PI_controller(double e_now, double e_int, double k_P, double k_I)
+  {
+    short = u; 
+    u = short(k_P*e_now+k_I*e_int); 
+    if (u>255)
+    {
+      u = 255;
+    }
+    else if (u<-255)
+    {
+      u = -255;
+    }
+    return u; 
+  }
+  u_L = k_P(v_l)
 
 }
